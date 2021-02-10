edaf2e8abc94597c2d19781cca2499c6da4ecbee
lukvmil <millerluke1364@gmail.com>
2021-02-10 15:52:55 -0500

    setting up new data base structure

--- agreements/app/database/metadata.py
+++ agreements/app/database/metadata.py
@@ -1,5 +1,6 @@
 from tinydb.database import Document
 from tinydb import where
+import json
 
 # container for metadata in the tinydb database
 class Metadata:
@@ -8,20 +9,22 @@ class Metadata:
         self.table = db.table('metadata')
 
         # initializes metadata table in database if it hasn't been created yet
+        self.initialize_database()
+
+    def initialize_database(self):
+        config = json.load(open('app/database/default_config.json', 'r'))
+        config['last_status_parsed'] = config['genesis_status']
+
         if not self.table.contains(doc_id=1):
-            self.table.insert(Document(
-                {
-                    'last_status_parsed': 1
-                }, 
-                doc_id=1))
+            self.table.insert(Document(config, doc_id=1))
 
     # retrieves a value from the metadata dictionary
     def retrieve(self, tag):
-        return self.table.get(doc_id=1)[tag]
+        return int(self.table.get(doc_id=1)[tag])
 
     # updates a value in the metadata dictionary
     def update(self, tag, value):
         self.table.update(
-            {tag: value},
+            {tag: str(value)},
             doc_ids=[1]
         )
