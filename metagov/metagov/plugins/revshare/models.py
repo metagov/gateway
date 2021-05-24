5f2805becaf471e868bcf78c5827c12ce7de3b46
Miriam Ashton <miriamashton3@gmail.com>
2021-05-24 15:45:47 -0400

    Revert revshare changes move to another branch

--- metagov/metagov/plugins/revshare/models.py
+++ metagov/metagov/plugins/revshare/models.py
@@ -1,14 +1,13 @@
+import json
 import logging
 import random
 
 import metagov.core.plugin_decorators as Registry
 import metagov.plugins.revshare.schemas as Schemas
-from metagov.core.errors import PluginErrorInternal
 from metagov.core.models import Plugin
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_KEY = "_DEFAULT"
 
 @Registry.plugin
 class RevShare(Plugin):
@@ -20,68 +19,60 @@ class RevShare(Plugin):
     def initialize(self):
         # This state only lasts as long as the plugin does.
         # If the community decides to de-activates the plugin, the plugin instance is deleted and the state is lost.
-        self.state.set(DEFAULT_KEY, {})
+        self.state.set("pointers", {})
 
     @Registry.action(
         slug="add-pointer",
         description="Add weighted pointer to revshare config, or update its weight if it already exists",
-        input_schema=Schemas.add_pointer_input,
+        input_schema=Schemas.pointer_and_weight,
     )
     def add_pointer(self, parameters):
         pointer = parameters["pointer"]
         weight = parameters["weight"]
-        key = parameters.get("key", DEFAULT_KEY)
-        config = self.state.get(key, {})
+
+        config = self.state.get("pointers")
         config[pointer] = weight
-        self.state.set(key, config)
+        self.state.set("pointers", config)
         return config
 
     @Registry.action(
-        slug="remove-pointer",
-        description="Remove pointer from revshare config",
-        input_schema=Schemas.remove_pointer_input
+        slug="remove-pointer", description="Remove pointer from revshare config", input_schema=Schemas.pointer
     )
     def remove_pointer(self, parameters):
         pointer = parameters["pointer"]
-        key = parameters.get("key", DEFAULT_KEY)
-        config = self.state.get(key, {})
+        config = self.state.get("pointers")
         config.pop(pointer, None)
-        self.state.set(key, config)
+        self.state.set("pointers", config)
         return config
 
     @Registry.action(
         slug="replace-config",
         description="Replace revshare config with new config",
-        input_schema=Schemas.replace_config_input,
+        input_schema=Schemas.pointers,
     )
     def replace(self, parameters):
         new_pointers = parameters["pointers"]
-        key = parameters.get("key", DEFAULT_KEY)
-        self.state.set(key, new_pointers)
+        self.state.set("pointers", new_pointers)
         return new_pointers
 
     @Registry.action(
         slug="get-config",
         description="Get current revshare configuration",
-        input_schema=Schemas.get_config_input,
         is_public=True,
     )
     def get_config(self, parameters):
-        key = parameters.get("key", DEFAULT_KEY)
-        return self.state.get(key, {})
+        return self.state.get("pointers")
 
     @Registry.action(
         slug="pick-pointer",
         description="Choose a random pointer according to weights",
-        input_schema=Schemas.pick_pointer_input,
-        output_schema=Schemas.pick_pointer_output,
+        output_schema=Schemas.pointer,
         is_public=True,
     )
     def pick_pointer(self, parameters):
-        key = parameters.get("key", DEFAULT_KEY)
-        pointers = self.state.get(key, {})
+        pointers = self.state.get("pointers")
         if len(pointers) == 0:
-            raise PluginErrorInternal(f"No pointers for key {key}")
+            raise Exception("No pointers")
         # based on https://webmonetization.org/docs/probabilistic-rev-sharing/
         sum_ = sum(list(pointers.values()))
         choice = random.random() * sum_
--- metagov/metagov/plugins/revshare/schemas.py
+++ metagov/metagov/plugins/revshare/schemas.py
@@ -1,32 +1,27 @@
-add_pointer_input = {
+pointer_and_weight = {
     "type": "object",
     "additionalProperties": False,
-    "properties": {"pointer": {"type": "string"}, "weight": {"type": "integer"}, "key": {"type": "string"}},
+    "properties": {"pointer": {"type": "string"}, "weight": {"type": "integer"}},
     "required": ["pointer", "weight"],
 }
-remove_pointer_input = {
-    "type": "object",
-    "additionalProperties": False,
-    "properties": {"pointer": {"type": "string"}, "key": {"type": "string"}},
-    "required": ["pointer"],
-}
-replace_config_input = {
-    "type": "object",
-    "additionalProperties": False,
-    "properties": {"pointers": {"type": "object"}, "key": {"type": "string"}},
-    "required": ["pointers"],
-}
-get_config_input = {
-    "type": "object",
-    "additionalProperties": False,
-    "properties": {"key": {"type": "string"}}
-}
-
-pick_pointer_input = get_config_input
-
-pick_pointer_output = {
+pointer = {
     "type": "object",
     "additionalProperties": False,
     "properties": {"pointer": {"type": "string"}},
     "required": ["pointer"],
 }
+
+pointers = {
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {"pointers": {"type": "object"}},
+    "required": ["pointers"],
+}
+
+lock_post_parameters = {
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {"id": {"type": "integer"}, "locked": {"type": "boolean"}},
+    "required": ["id", "locked"],
+}
+lock_post_response = {"type": "object", "additionalProperties": False, "properties": {"locked": {"type": "boolean"}}}
