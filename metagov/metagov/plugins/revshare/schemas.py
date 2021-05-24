612ba551b86a61bc69f1136ad77a252d8fa3aa55
Miriam Ashton <miriamashton3@gmail.com>
2021-05-24 15:45:04 -0400

    Support multiple revshare configs by key

--- metagov/metagov/plugins/revshare/schemas.py
+++ metagov/metagov/plugins/revshare/schemas.py
@@ -1,27 +1,32 @@
-pointer_and_weight = {
+add_pointer_input = {
     "type": "object",
     "additionalProperties": False,
-    "properties": {"pointer": {"type": "string"}, "weight": {"type": "integer"}},
+    "properties": {"pointer": {"type": "string"}, "weight": {"type": "integer"}, "key": {"type": "string"}},
     "required": ["pointer", "weight"],
 }
-pointer = {
+remove_pointer_input = {
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {"pointer": {"type": "string"}, "key": {"type": "string"}},
+    "required": ["pointer"],
+}
+replace_config_input = {
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {"pointers": {"type": "object"}, "key": {"type": "string"}},
+    "required": ["pointers"],
+}
+get_config_input = {
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {"key": {"type": "string"}}
+}
+
+pick_pointer_input = get_config_input
+
+pick_pointer_output = {
     "type": "object",
     "additionalProperties": False,
     "properties": {"pointer": {"type": "string"}},
     "required": ["pointer"],
 }
-
-pointers = {
-    "type": "object",
-    "additionalProperties": False,
-    "properties": {"pointers": {"type": "object"}},
-    "required": ["pointers"],
-}
-
-lock_post_parameters = {
-    "type": "object",
-    "additionalProperties": False,
-    "properties": {"id": {"type": "integer"}, "locked": {"type": "boolean"}},
-    "required": ["id", "locked"],
-}
-lock_post_response = {"type": "object", "additionalProperties": False, "properties": {"locked": {"type": "boolean"}}}
