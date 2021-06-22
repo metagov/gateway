cc40e1ec49b5f7406f1eecfa059d700fbc9d1a85
Luke Miller <millerluke1364@gmail.com>
2021-06-22 14:27:18 -0400

    added all get api calls for tsc (also removed old gitignores for agreement engine):

--- metagov/metagov/plugins/tsc/models.py
+++ metagov/metagov/plugins/tsc/models.py
@@ -19,24 +19,69 @@ class TSC(Plugin):
 
     @Registry.action(
         slug="get-user",
-        description="Returns information about a requested user",
+        description="Returns information about the requested user",
         input_schema={
             "type": "object",
             "properties": {
                 "user_id": {"type": "string"}
             },
             "required": ["user_id"]
-        },
-        output_schema={
-            "type": "object",
-            "properties": {
-                "balance": {"type": "string"},
-                # "contracts": {"type": "list"}
-            }
         }
     )
     def get_user(self, data):
         url = self.config['server_url'] + '/api/user/' + data['user_id']
+        response = requests.get(url)
+
+        return response.json()
+
+
+    @Registry.action(
+        slug='get-contract',
+        description='Returns information about the requested contract',
+        input_schema={
+            'type': 'object',
+            'properties': {
+                'contract_id': {'type': 'string'}
+            },
+            'required': ['contract_id']
+        }
+    )
+    def get_contract(self, data):
+        url = self.config['server_url'] + '/api/contract/' + data['contract_id']
+        response = requests.get(url)
+
+        return response.json()
+    
+    @Registry.action(
+        slug='get-execution',
+        description='Returns information about the requested execution',
+        input_schema={
+            'type': 'object',
+            'properties': {
+                'execution_id': {'type': 'string'}
+            },
+            'required': ['execution_id']
+        }
+    )
+    def get_execution(self, data):
+        url = self.config['server_url'] + '/api/execution/' + data['execution_id']
+        response = requests.get(url)
+
+        return response.json()
+    
+    @Registry.action(
+        slug='get-agreement',
+        description='Returns information about the requested agreement',
+        input_schema={
+            'type': 'object',
+            'properties': {
+                'agreement_id': {'type': 'string'}
+            },
+            'required': ['agreement_id']
+        }
+    )
+    def get_agreement(self, data):
+        url = self.config['server_url'] + '/api/agreement/' + data['agreement_id']
         response = requests.get(url)
 
         return response.json()
