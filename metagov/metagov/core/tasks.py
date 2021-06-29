e0bb6df3168eb32506f57794bd78b018a644c013
Miriam Ashton <miriamashton3@gmail.com>
2021-06-29 10:40:18 -0400

    Don't retry Slack messages after timeout, and logging update (#84)

--- metagov/metagov/core/tasks.py
+++ metagov/metagov/core/tasks.py
@@ -15,7 +15,7 @@ def execute_plugin_tasks():
         if cls._task_function:
             active_plugins = cls.objects.all()
             if active_plugins.count() > 0:
-                logger.debug(f"[tasks] calling task function for {active_plugins.count()} instances of {plugin_name}")
+                logger.debug(f"Calling task function for {active_plugins.count()} instances of {plugin_name}")
             for plugin in active_plugins:
                 task_function = getattr(plugin, cls._task_function)
                 task_function()
@@ -23,9 +23,9 @@ def execute_plugin_tasks():
         # invoke all the governance process pending task checkers
         for (process_name, process_cls) in cls._process_registry.items():
             active_processes = process_cls.objects.filter(status=ProcessStatus.PENDING.value)
+            if active_processes.count() > 0:
+                logger.debug(f"Calling update function for {active_processes.count()} pending {process_name} processes")
             for process in active_processes:
-                logger.debug(f"[tasks] updating process: '{process}'")
-
                 # Invoke `update`. It may lead to the outcome or status being changed,
                 # which will send a callback notification to the Driver from the `pre_save signal`
                 try:
