110e336164eb0f39fdc7f6c958f6bf2055404006
Luke Miller <millerluke1364@gmail.com>
2021-08-11 11:14:55 -0400

    added direct message action to twitter plugin

--- metagov/metagov/plugins/twitter/models.py
+++ metagov/metagov/plugins/twitter/models.py
@@ -70,6 +70,25 @@ class Twitter(Plugin):
         # logger.debug(res._json)
         return res
 
+    @Registry.action(
+        slug="send-dm",
+        description="Send a direct message",
+        input_schema={
+            "type": "object",
+            "properties": {
+                "user_id": {"type": "string"},
+                "text": {"type": "string"}
+            },
+            "required": ["user_id", "text"]
+        }
+    )
+    def send_direct_message(self, parameters):
+        user_id = int(parameters['user_id'])
+        text = parameters['text']
+        
+        res = self.tweepy_api().send_direct_message(user_id, text)
+        return res
+
     @Registry.event_producer_task()
     def my_task_function(self):
         api = self.tweepy_api()
