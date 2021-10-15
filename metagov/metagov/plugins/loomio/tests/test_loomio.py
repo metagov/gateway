74728e073057fc06207a09298f17e72e033c82cc
Miriam Ashton <miriamashton3@gmail.com>
2021-10-15 14:44:15 -0400

    Update Loomio vote outcome shape to match Slack/OC/GitHub vote processes

--- metagov/metagov/plugins/loomio/tests/test_loomio.py
+++ metagov/metagov/plugins/loomio/tests/test_loomio.py
@@ -3,8 +3,9 @@ from django.test import TestCase
 import metagov.plugins.loomio.tests.mocks as LoomioMock
 
 
-
 class UnitTests(TestCase):
     def test_vote_dict(self):
         vote_dict = create_vote_dict(LoomioMock.loomio_show_poll_response)
-        self.assertDictEqual(vote_dict, {"agree": 1, "disagree": 0})
+        self.assertDictEqual(
+            vote_dict, {"agree": {"count": 1, "users": ["879750"]}, "disagree": {"count": 0, "users": []}}
+        )
