1e9dc45ddb6c08089ad57e73b24e283fdc4b3e9c
Shauna Gordon-McKeon <shaunagm@gmail.com>
2021-11-15 10:48:45 -0500

    Add missing Tags import

--- metagov/metagov/httpwrapper/urls.py
+++ metagov/metagov/httpwrapper/urls.py
@@ -6,6 +6,7 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 from metagov.core import utils
+from metagov.httpwrapper.openapi_schemas import Tags
 from metagov.httpwrapper import views, utils
 from metagov.httpwrapper import identity as identity_views
 from metagov.core.plugin_manager import plugin_registry
